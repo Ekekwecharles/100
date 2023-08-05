a9648f853704c4a9504115ed43cfb37bd98ceb91
Charlessnow <ekekwecharles50@gmail.com>
2023-08-05 10:41:46 +0100

    Day 11
    So today I added participants to each room(This has a many to many relationship with the user. This means a User can be in many rooms and A room can also have multiple Users/participants).
    More message functionality(only owners of a message can  delete their message)
    Refactored my Home page html code into  components, the home page was becoming stuffy.
    When you search for a specific room, messages are filtered to show only messages from that room.

---
+++ day11/studybud/base/views.py
@@ -0,0 +1,187 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from .models import Room, Topic, Message
+from .forms import RoomForm
+
+# Create your views here.
+
+# rooms = [
+#     {'id': 1, 'name': 'lets learn python'},
+#     {'id': 2, 'name': 'Design with me'},
+#     {'id': 3, 'name': 'Frontend dev'},
+# ]
+
+
+def loginPage(request):
+    page = "login"
+
+    if request.user.is_authenticated:
+        return redirect("home")
+
+    if request.method == "POST":
+        username = request.POST.get("username").lower()
+        password = request.POST.get("password")
+
+        # Checks if the user exist
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "User does not exist")
+
+        # Checks if the credentials are correct
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Username or Password does not exist")
+
+    context = {"page": page}
+    return render(request, "base/login_register.html", context)
+
+
+def logoutUser(request):
+    # basically what this does in simple terms is delete the session id from the browser
+    # like the one stored in cookies in the browser
+    logout(request)
+    return redirect("home")
+
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # commit=false means we want to user the user object immediately
+            # User will get create but we will access it right away
+            user = form.save(commit=False)
+            # we want to clean up the data
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "An error occured during registration")
+    return render(request, "base/login_register.html", {"form": form})
+
+
+def home(request):
+    q = request.GET.get("q") if request.GET.get("q") != None else ""
+    # Without the icontains, the All link in the home page wont function well
+    # icontains makes it possible even if you search for ja in javascript, you will get all the javascript topics
+    # therefore when nothing is passed(and empty string) its going to match everything
+    # The 'i' means case insensitive
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q)
+    )
+    topics = Topic.objects.all()
+    # count() gets the length of a query set
+    room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+    context = {
+        "rooms": rooms,
+        "topics": topics,
+        "room_count": room_count,
+        "room_messages": room_messages,
+    }
+    return render(request, "base/home.html", context)
+
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    # for one to many, we use '_set' _set.all() gets all the messages relatled to that room
+    room_messages = room.message_set.all()  # .order_by('-created')
+    # for many to many we use just all()
+    participants = room.participants.all()
+
+    if request.method == "POST":
+        message = Message.objects.create(
+            user=request.user, room=room, body=request.POST.get("body")
+        )
+        room.participants.add(request.user)
+        return redirect("room", pk=room.id)
+
+    context = {
+        "room": room,
+        "room_messages": room_messages,
+        "participants": participants,
+    }
+    return render(request, "base/room.html", context)
+
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    #getting all the rooms a user is in
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {
+        "user": user,
+        'rooms': rooms,
+        'room_messages': room_messages,
+        'topics': topics
+        }
+    return render(request, "base/profile.html", context)
+
+
+@login_required(login_url="login")
+def createRoom(request):
+    form = RoomForm()
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
+
+
+@login_required(login_url="login")
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    # This populates the form with intial values in this case values
+    # we just got from the querying the database above
+    form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse("You are not allowed here!")
+
+    if request.method == "POST":
+        # This replaces the old values(room) with the new one form request.POST
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
+
+
+@login_required(login_url="login")
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == "POST":
+        room.delete()
+        return redirect("home")
+    return render(request, "base/delete.html", {"obj": room})
+
+
+@login_required(login_url="login")
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse("Your are not allowed here!!")
+
+    if request.method == "POST":
+        message.delete()
+        return redirect("home")
+    return render(request, "base/delete.html", {"obj": message})
